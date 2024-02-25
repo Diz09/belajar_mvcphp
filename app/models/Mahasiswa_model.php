f7efc72102822db9744e221325c11359785d94e3
Dimas <124556325+Diz09@users.noreply.github.com>
2024-02-25 09:33:59 +0700

    mvc8dbWrapper

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -2,52 +2,25 @@
 
 class Mahasiswa_model {
 
-    private $dbh; //database handler
-    private $stmt;
+    private $table = 'mahasiswa';
+    private $db;
 
-    public function __construct() {
-        //data source name
-        $dsn = 'mysql:host=localhost;dbname=belajar_phpmvc';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch (PDOException $e) { 
-            die($e->getMessage());
-        }
+    public function __construct()
+    {
+        $this->db = new Database;
     }
 
-    // private $mhs = [
-    //     [
-    //         "nama" => "Gilang",
-    //         "nim" => "e4122xx1",
-    //         "jurusan" => "TI"
-    //     ],
-    //     [
-    //         "nama" => "Bayu",
-    //         "nim" => "e4122xx2",
-    //         "jurusan" => "TI"
-    //     ],
-    //     [
-    //         "nama" => "Dani",
-    //         "nim" => "e4122xx3",
-    //         "jurusan" => "TI"
-    //     ],
-    //     [
-    //         "nama" => "Dimas",
-    //         "nim" => "e4122xx4",
-    //         "jurusan" => "TI"
-    //     ],
-    //     [
-    //         "nama" => "Rizki",
-    //         "nim" => "e4122xx5",
-    //         "jurusan" => "TI"
-    //     ],
-    // ];
-
-    public function getAllMahasiswa() {
-        // return $this->mhs;
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function getAllMahasiswa() 
+    {
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
     }
-}
+
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' .$this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
+
+} 
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -6,13 +6,14 @@
 
             <h3>Daftar Mahasiswa</h3>
 
-            <?php foreach($data['mhs'] as $mhs) : ?>
-                <ul>
-                    <li><?= $mhs['nama'];?></li>
-                    <li><?= $mhs['nim'];?></li>
-                    <li><?= $mhs['jurusan'];?></li>
+                <ul class="list-group">
+                    <?php foreach($data['mhs'] as $mhs) : ?>
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            <?= $mhs['nama'];?>
+                            <a href="<?= BASEURL; ?>/mahasiswa/detail/ <?= $mhs['id']?>" class="badge badge-primary">detail</a>
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
-            <?php endforeach; ?>
 
         </div>
 
