deaba8aae535dcd53e70fcf711e62b61648d2dc5
Dimas <124556325+Diz09@users.noreply.github.com>
2024-05-14 20:31:33 +0700

    fin

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('BASEURL', 'http://localhost/phpmvc/public');
+define('BASEURL', 'http://localhost:8080/phpmvc/public');
 
 // DB
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:8080');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'belajar_phpmvc');
